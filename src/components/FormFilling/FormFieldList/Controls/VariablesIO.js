1ff86830f690f39bbd2e4cfe80c2d94ae7c5cee7
Daniele Plantera <tango1266@arcor.de>
2021-07-25 13:47:48 +0200

    Extracted common component

--- src/components/FormFilling/FormFieldList/Controls/VariablesIO.js
+++ src/components/FormFilling/FormFieldList/Controls/VariablesIO.js
@@ -1,14 +1,15 @@
 import React, {useState} from 'react';
-import UploadVariables from "./UploadVariables";
 import {Button, TextField} from "@material-ui/core";
 import {ClientDownload} from "../../../../utils/ClientDownload";
 import {useStore} from "../../../../store";
+import {FormVariable} from "../../../../model/types";
+import UploadDialog from "../../../commons/UploadDialog";
 
 const downloadClient = new ClientDownload();
 
 const VariablesIO = () => {
     const [fileName, setFileName] = useState("variables")
-    const [variables, updateVariables] = useStore( state => [state.variables, state.updateVariables])
+    const [variables, updateVariables] = useStore(state => [state.variables, state.updateVariables])
 
     const downloadCsv = (e, variables) => {
         const rows = variables.map(variable => {
@@ -22,6 +23,61 @@ const VariablesIO = () => {
         downloadClient.forCsv.download(rows, fileName);
     }
 
+    function handleUploadCsv(text, filename, options) {
+        //name;value;desc;example
+        const rows = text.split("\n");
+        const vars = rows.reduce((result, row) => {
+            const [name, value, desc, example] = row.split(";");
+
+            const isEmpty = (str) => {
+                return str === null || !(str?.length > 0);
+            }
+
+            if (isEmpty(name) && isEmpty(value))
+                return result;
+
+            const getVarCandidate = (name, value) => {
+                if (!isEmpty(name) && isEmpty(value)) return FormVariable(name, name, desc, example);
+                if (isEmpty(name) && !isEmpty(value)) return FormVariable(value, value, desc, example);
+                return FormVariable(name, value, desc, example);
+
+            }
+            result.push(getVarCandidate(name, value));
+
+            return result;
+        }, [])
+
+        const ignored = [];
+        const overwritten = [];
+        const newVariables = [];
+        vars.forEach(varCandidate => {
+            const isEqual = (candidate, other) => {
+                if (!other) return false;
+                return candidate.id === other.id || candidate.value === other.value;
+            }
+            const idxExisting = variables.findIndex(existVar => isEqual(varCandidate, existVar));
+            const isNewVariable = idxExisting === -1;
+            const ignoreVariable = idxExisting >= 0 && !options.overwriteExisting?.value;
+            if (isNewVariable) {
+                newVariables.push(varCandidate);
+            } else if (!ignoreVariable) {
+                const existing = variables[idxExisting];
+                overwritten.push(existing);
+                variables[idxExisting] = {...existing, ...varCandidate};
+            } else if (ignoreVariable) {
+                ignored.push(varCandidate);
+            }
+        })
+
+        if(newVariables.length <= 0 && overwritten.length <= 0) {
+            console.info("VariablesIO: variables up-to-date.", {newVariables, overwritten, ignored})
+            return
+        }
+
+        console.info("VariablesIO: done uploading.", {newVariables, overwritten, ignored})
+        updateVariables([...variables, ...newVariables]);
+    }
+
     return (
         <div style={{position: "relative", display: "flex", width: "100%", alignItems: "center", gap: " 20px"}}>
             <TextField id="vars-export-name" label={`Variables export name`} defaultValue={fileName + ".csv"}
@@ -31,7 +87,18 @@ const VariablesIO = () => {
                        }}/>
             <Button id="btn-vars-download" size={"small"} style={{height: "50%"}}
                     onClick={(e) => downloadCsv(e, variables)}>Download</Button>
-            <UploadVariables formVariables={variables} addVariables={updateVariables}/>
+            <UploadDialog handleUpload={handleUploadCsv}
+                          uploadOptions={{
+                              overwriteExisting: {
+                                  value: false,
+                                  label: "Bestehende Variablen überschreiben?"
+                              },
+                              test: {
+                                  value: false,
+                                  label: "test?"
+                              }
+                          }}
+                          title={"Variablen Hochladen"}/>
         </div>
     );
 };
---
+++ src/components/commons/UploadDialog.js
@@ -0,0 +1,117 @@
+import React, {useState} from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import {useTheme} from '@material-ui/core/styles';
+import {Checkbox, FormControlLabel, Typography} from "@material-ui/core";
+import {ClientUpload} from "../../utils/ClientUpload";
+
+export default function UploadDialog({handleUpload, uploadOptions, title}) {
+    const [open, setOpen] = React.useState(false);
+    const [options, setOptions] = useState({...uploadOptions});
+    const theme = useTheme();
+    const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
+    const clientUpload = new ClientUpload();
+    const varDrop = () => {
+        return document.getElementById("drop-zone-var")
+    };
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleCheckedOption = (e) => {
+        console.debug({e, target: e.target, value: e.target.value, checked: e.target.checked})
+        const name = e.target.name;
+
+        if (!Object.keys(options).includes(name)) {
+            console.warn("UploadDialog: no option found for " + name);
+            return;
+        }
+        options[name].value = e.target.checked;
+        setOptions({...options});
+    }
+
+    const handleOpenFile = (e) => {
+        clientUpload.forFilePicker.uploadAsText(e, (text, fileName) => {
+            handleUpload(text, fileName, options);
+        })
+        handleClose();
+    };
+
+    const handleDropFile = (e) => {
+        //https://stackoverflow.com/questions/22048395/how-to-open-a-local-var-in-pdfjs-using-file-input
+        e.preventDefault();
+        varDrop().style.backgroundColor = "none";
+        setOpen(false);
+
+        clientUpload.forDropEvent.uploadAsText(e, (text, fileName) => {
+            handleUpload(text, fileName, options);
+        })
+    }
+
+    const renderOptions = () => {
+        return Object.keys(options).map((optionKey, index) => {
+            const opt = options[optionKey];
+            const optName = optionKey;
+            return <FormControlLabel key={index}
+                                     control={<Checkbox checked={opt.value} onChange={handleCheckedOption}
+                                                        name={optName}/>}
+                                     label={opt.label}/>
+        })
+    }
+
+    return (
+        <div>
+            <Button onClick={handleClickOpen} size={"small"} style={{height: "50%"}}>
+                Upload
+            </Button>
+            <Dialog
+                fullScreen={fullScreen}
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="upload-var-title"
+            >
+                <DialogTitle id="upload-var-title">{title}</DialogTitle>
+
+                <DialogContent>
+                    {renderOptions()}
+                    <input type="file" id="file" name="file" encType="multipart/form-data" hidden={true}/>
+                    <div className={"drop-zone"} id={"drop-zone-var"} style={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        width: "400px",
+                        height: "200px"
+                    }}
+                         onDragOver={(e) => {
+                             e.preventDefault();
+                             varDrop().style.backgroundColor = "gray";
+                             varDrop().style.opacity = "60%"
+                         }}
+                         onDrop={handleDropFile}
+                    >
+                        <Typography>Drag and Drop</Typography>
+                    </div>
+                </DialogContent>
+                <DialogActions>
+                    <label htmlFor="var-file-upload">
+                        <Button variant="contained" color="primary" component="span">
+                            Upload Variables
+                        </Button>
+                    </label>
+                    <input accept=".csv" style={{display: "none"}} id="var-file-upload" type="file"
+                           onChange={handleOpenFile}/>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
+}
