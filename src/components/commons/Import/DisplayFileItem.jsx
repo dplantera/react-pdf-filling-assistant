bae2c85623e18e5540663403207e34a4776f3277
Daniele Plantera <28092959+dplantera@users.noreply.github.com>
2021-08-25 13:01:28 +0200

    Fix react warning

--- src/components/commons/Import/DisplayFileItem.jsx
+++ src/components/commons/Import/DisplayFileItem.jsx
@@ -24,8 +24,8 @@ const DisplayFileItem = ({itemId, fileName, fileSize, fileType, onChecked, onAct
     const byteToMb = bytesToMegaBytes(fileSize ?? 0, 4);
     const classes = useStyles();
 
-    const handleCheckedChange = useCallback((e) => onChecked?.(e.target.checked, fileName, fileSize), [onChecked, itemId, fileName, fileSize]);
-    const handleAction = useCallback(() => onAction?.(fileName, fileSize), [onAction, itemId, fileName, fileSize]);
+    const handleCheckedChange = useCallback((e) => onChecked?.(e.target.checked, fileName, fileSize), [onChecked, fileName, fileSize]);
+    const handleAction = useCallback(() => onAction?.(fileName, fileSize), [onAction, fileName, fileSize]);
 
     return (
         <ListItem classes={{container: classes.container}} divider>
