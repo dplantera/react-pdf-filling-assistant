6d2091eeeb42a9ced70d3db36dbb6d1efdc0499d
Daniele Plantera <tango1266@arcor.de>
2021-10-23 12:42:22 +0200

    Feat(RadioBtn): Export group as single field

--- src/components/actions/exportActions.js
+++ src/components/actions/exportActions.js
@@ -3,6 +3,18 @@ import {FieldTypes} from "../../model/types";
 
 const downloadClient = new ClientDownload();
 
+const RuleTypesCSV = {
+    CELL: {name: "cell"}
+}
+
+const RuleTypes = {
+    ...FieldTypes,
+    ...RuleTypesCSV,
+    FIELD_VALUE: {name: "value"},
+    FIELD_DESCRIPTION: {name: "description"},
+    FIELD_NAME: {name: "name"},
+}
+
 // $[ '$!att.var' == '$!test', $var1, $var2 ]
 const exportOptions = {
     serialization: {
@@ -10,42 +22,45 @@ const exportOptions = {
         multiFieldExport: false,
         csvRules: [
             {
-                name: "no double quotes in rows",
-                type: "row",
+                name: "no double quotes in values",
+                type: RuleTypesCSV.CELL,
                 validate: (row) => !row.includes("\""),
                 fix: (row) => row.replaceAll("\"", '\'')
+            },
+            {
+                name: "no multiline values",
+                type: RuleTypesCSV.CELL,
+                validate: (value) => /\r?\n|\r/g.test(value),
+                fix: (value) => value.replaceAll(/\r?\n|\r/g, '')
             }
         ],
         fieldRules: [
             {
-                name: "no multiline values",
-                type: 'value',
-                validate: (value) => /\r?\n|\r/g.test(value),
-                fix: (value) => value.replaceAll(/\r?\n|\r/g, '')
-            },
-            {
                 name: "group fields in single field",
-                type: FieldTypes.RADIO.name,
+                type: RuleTypes.RADIO,
                 template: (groupFieldValues) => `$[${groupFieldValues.map(field => field || 'false').join(",")}]`
             }
         ]
     }
-
 }
 
-const getVariable = (field, variables) => variables.find(v => v.id === field.variable);
+function areEqualRuleTypes(typeA, typeB) {
+    if (!typeA || !typeB) return false;
+    if (typeA === typeB) return true;
+    return typeA.name && typeA.name === typeB.name;
+}
 
 function applyRules(value, type, ruleSet, applyFixes) {
-    const rules = ruleSet?.filter(rule => rule?.type === "all" || rule?.type === type)
+    const rules = ruleSet?.filter(rule => areEqualRuleTypes(rule?.type, type));
     let validValue = value;
-    if(!validValue) return "";
+    if (!validValue) return "";
     rules.forEach(rule => {
         const isValidationRule = rule.validate !== undefined || rule.fix;
         if (isValidationRule && !rule?.validate?.(validValue)) {
-            applyFixes? console.debug("fixing: " + rule.name, {validValue}) : console.error(rule.name, {value, rule});
+            applyFixes ? console.debug("fixing: " + rule.name, {validValue}) : console.error(rule.name, {value, rule});
             validValue = applyFixes && rule.fix ? rule.fix(validValue) : validValue;
         }
-        if(rule.template) {
+        if (rule.template) {
             console.debug("applying template: " + rule.name);
             validValue = rule.template(validValue);
         }
@@ -54,55 +69,60 @@ function applyRules(value, type, ruleSet, applyFixes) {
 }
 
 const applyFieldRules = (val, type) => applyRules(val, type, exportOptions.serialization.fieldRules, exportOptions.serialization.applyFixes);
-
-function applyRadioRules(field, variables, fields) {
-    let shouldExportAsSingleField = !exportOptions.serialization.multiFieldExport;
-    const hasFieldChildren = field?.groupInfo?.children?.length > 0;
-    if(shouldExportAsSingleField && !hasFieldChildren)
-        return null;
-
-    const groupField = {...field};
-    const groupChildren = field.groupInfo.children.map(childId => fields.find( child => child.id === childId));
-
-    const getVal = (_field) => {
-        console.debug({_field})
-        return _field.variable ? getVariable(_field, variables)?.value ?? "" : _field.value || ""
-    };
-
-    if(shouldExportAsSingleField) {
-        console.log({groupField, groupChildren})
-        let groupValues = groupChildren.map(child => getVal(child));
-        groupField.value = applyFieldRules(groupValues, field.type.name);
-        groupField.type = FieldTypes.TEXT;
-        return applyFieldRulesDefault(groupField, variables, fields)
-    }
-    console.groupEnd();
-    return [field.name, getVal(field), ...groupChildren.map(child => [child.name, getVal(child) ])]
-}
-
-function applyFieldRulesDefault (field, variables, fields) {
-    let value = field.variable ? getVariable(field, variables)?.value ?? "" : field.value || "";
-    value = applyFieldRules(value, "value");
-    const fieldName = applyFieldRules(field.name, field.type?.name);
-    const fieldDesc = applyFieldRules(field.description, field.type?.name);
-    return [fieldName, value, fieldDesc]
-}
+const applyCsvRules = (val, type) => applyRules(val, type, exportOptions.serialization.csvRules, exportOptions.serialization.applyFixes);
+const getVariable = (field, variables) => variables.find(v => v.id === field.variable);
+const getValue = (field, variables) => field.variable ? getVariable(field, variables)?.value ?? "" : field.value || "";
 
 export function exportFieldListAsCsv(fieldList, variables, fields) {
     console.group("exportFieldListAsCsv")
     const fieldsInList = fields.filter(field => field.fieldListId === fieldList.id);
     const rows = fieldsInList.map(field => {
         switch (field.type.name) {
-            case FieldTypes.RADIO.name:
-                return applyRadioRules(field, variables, fields);
+            case RuleTypes.RADIO.name:
+                return applyCsvRulesRadioBtn(field, variables, fields);
             default:
-                return applyFieldRulesDefault(field, variables, fields);
+                return applyCsvRulesDefault(field, variables, fields);
         }
     })
-    const rowsValidated = rows.filter(row => row != null).map(row => row.map(cell => cell? applyRules(cell, "row", exportOptions.serialization.csvRules, exportOptions.serialization.applyFixes): cell));
+    const rowsValidated = rows.filter(row => row != null).map(row => row.map(cell => cell ? applyCsvRules(cell, RuleTypesCSV.CELL) : cell));
     downloadClient.forCsv
         .changeSettings({useBOM: true})
         .download(rowsValidated, fieldList.name);
     console.groupEnd()
+}
+
+function applyCsvRulesRadioBtn(field, variables, fields) {
+    console.group("applyCsvRulesRadioBtn");
+    let shouldExportAsSingleField = !exportOptions.serialization.multiFieldExport;
+    const hasFieldChildren = field?.groupInfo?.children?.length > 0;
+    if (shouldExportAsSingleField && !hasFieldChildren)
+        return null;
+
+    const groupField = {...field};
+    const groupChildren = field.groupInfo.children.map(childId => fields.find(child => child.id === childId));
+    try {
+        if (shouldExportAsSingleField) {
+            console.log({groupField, groupChildren})
+            let groupValues = groupChildren.map(child => getValue(child, variables));
+            groupField.value = applyFieldRules(groupValues, field.type);
+            groupField.type = FieldTypes.TEXT;
+            console.groupEnd();
+            return applyCsvRulesDefault(groupField, variables)
+        }
+        console.groupEnd();
+        return [field.name, getValue(field, variables), ...groupChildren.map(child => [child.name, getValue(child, variables)])]
+    } catch (error) {
+        console.error("csv rules failed: ", {groupField, error})
+    }
+}
+
+function applyCsvRulesDefault(field, variables) {
+    console.group("applyCsvRulesRadioBtn");
+    let value = getValue(field, variables);
+    value = applyFieldRules(value, RuleTypes.FIELD_VALUE);
+    const fieldName = applyFieldRules(field.name, RuleTypes.FIELD_NAME);
+    const fieldDesc = applyFieldRules(field.description, RuleTypes.FIELD_DESCRIPTION);
+    console.groupEnd();
+    return [fieldName, value, fieldDesc]
 }
 
